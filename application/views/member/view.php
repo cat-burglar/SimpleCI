f76d602d4848f8c6c6d1101e2097f7494b2176d2
cat-burglar <gaezeldorato@gmail.com>
2019-05-20 12:45:07 +0800

    clean merge

--- application/views/member/view.php
+++ application/views/member/view.php
@@ -1,23 +1,17 @@
-	<div class="member-view container-fluid">
-	 <div class="row profile">
+<div class="member-view container-fluid">
+	<div class="row profile">
 		<div class="col-md-3">
 			<div class="profile-sidebar border px-5 py-5">
 				<!-- SIDEBAR USERPIC -->
 				<div class="profile-userpic ">
 					<img src="<?php echo base_url();?>assets/images/profile.jpg" class="mx-auto d-block img-thumbnail img-responsive" alt="">
 				</div>
-				<!-- END SIDEBAR USERPIC -->
 
-				<!-- SIDEBAR USER TITLE -->
 				<div class="profile-usertitle">
 					<div class="profile-usertitle-job text-center mt-2">
 						Member since 2017
 					</div>
 				</div>
-				<!-- END SIDEBAR USER TITLE -->
-				<!-- SIDEBAR BUTTONS -->
-				
-				<!-- END SIDEBAR BUTTONS -->
 
 				<!-- SIDEBAR MENU -->
 				<div class="profile-usermenu mt-3 border-top border-primary">
@@ -35,20 +29,7 @@
 				    
 					<?php 
 						$type = $this->session->userdata('type');
-<<<<<<< HEAD
-						if($type == '3') {
-					?>
-					<div class="row mx-auto">
-					    <p class="mx-auto"> <strong> Rating: </strong></p>   
-					</div>
 
-					<div class="star-rating">
-						<span class="fa fa-star checked"></span>
-						<span class="fa fa-star checked"></span>
-						<span class="fa fa-star checked"></span>
-						<span class="fa fa-star"></span>
-						<span class="fa fa-star"></span>
-=======
 						if($type == '2') {
 					?>
 					<div class="row mx-auto">
@@ -60,7 +41,6 @@
 						<div class="stars-outer">
 				          <div class="stars-inner"></div>
 				        </div>
->>>>>>> master
 					</div>
 
 					<?php } ?>
@@ -82,8 +62,5 @@
 				
             </div>
 		</div>
-<<<<<<< HEAD
 	</div>
-=======
-	</div>
->>>>>>> master
+</div>
